5f69da86acce95708a2e6a5b64cdf1057f8e2b6d
hmzdnzl <hmzdnzl38@gmail.com>
2025-11-07 02:04:00 +0300

    top 100 film siteye eklendi

--- src/pages/Top100.jsx
+++ src/pages/Top100.jsx
@@ -6,21 +6,44 @@ export default function Top100() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axiosInstance.get('/movie/popular')
-      .then(response => {
-        setMovies(response.data.results);
-      })
-      .catch(error => {
-        console.error("Error fetching top 100 movies:", error);
-      });
+    const fetchMovies = async () => {
+      try {
+        const requests = [];
+        for (let page = 1; page <= 5; page++) {
+          requests.push(
+            axiosInstance.get('/movie/top_rated', {
+              params: {
+                language: 'en-US',
+                page: page,
+              },
+            })
+          );
+        }
+        const responses = await Promise.all(requests);
+        const allMovies = responses.flatMap(res => res.data.results);
+        setMovies(allMovies);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+    fetchMovies();
   }, []);
-
+  console.log(movies);
   return (
     <section>
         <div><h1>Top 100 Movies</h1></div>
-        <div>
-           
+        <div className='flex flex-col pl-4 gap-y-3'>
+          <h2>Movie Names</h2>
+<div>
+          {movies.map(movie => (
+            <div className='flex flex-col gap-y-1' key={movie.id}>
+              <h2>{movie.title}</h2>
+              
+            </div>
+          ))}
         </div>
+        </div>
+        
       
     </section>
   );
